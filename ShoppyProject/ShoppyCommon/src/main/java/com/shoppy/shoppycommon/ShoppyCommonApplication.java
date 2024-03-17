fe6429a1e48a060bc7621f0aaf95cfd1ff0e75b3
phong <darkmirror2000@gmail.com>
2024-03-17 09:20:56 +0700

    settup structure

---
+++ ShoppyProject/ShoppyCommon/src/main/java/com/shoppy/shoppycommon/ShoppyCommonApplication.java
@@ -0,0 +1,13 @@
+package com.shoppy.shoppycommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppyCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppyCommonApplication.class, args);
+    }
+
+}
